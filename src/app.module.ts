37d40e9bb8c591a7dcf94678ea9212bf411afba4
Sebastián Guzmán <iguzman@viamatica.me>
2022-08-07 14:27:16 -0500

    setup env and database orm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,29 @@
+import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
+import { configuration, validationSchema } from './../config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    ConfigModule.forRoot({
+      envFilePath: `config/env/.${process.env.NODE_ENV}.env`,
+      isGlobal: true,
+      load: [configuration],
+      validationSchema,
+    }),
+    TypeOrmModule.forRoot({
+      type: process.env.DATABASE_TYPE,
+      host: process.env.DATABASE_HOST,
+      port: +process.env.DATABASE_PORT,
+      database: process.env.DATABASE_NAME,
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })

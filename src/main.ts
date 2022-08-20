dd26831f25dd98ae17deafe4002fec51aeee5455
Sebastián Guzmán <ivansebastiangf@gmail.com>
2022-08-20 18:35:20 -0500

    Feat(global): Add global pipes

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import GlobalPipesArray from './global-pipes';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+
+  app.useGlobalPipes(...GlobalPipesArray);
 
   await app.listen(80);
 }

13ee293e7e21d97c32666eb01809eb2e1b616365
Sebastián Guzmán <iguzman@viamatica.me>
2022-09-12 22:13:05 -0500

    Token authentication

--- src/users/services/auth.service.ts
+++ src/users/services/auth.service.ts
@@ -5,6 +5,7 @@ import {
   UnauthorizedException,
 } from '@nestjs/common';
 import { Repository } from 'typeorm';
+import { JwtService } from '@nestjs/jwt';
 
 import { comparePasswords } from '../utils/password-manager';
 import { User } from '../entities/user.entity';
@@ -14,6 +15,7 @@ import { UserLoginDTO } from '../dtos';
 export class AuthService {
   public constructor(
     @InjectRepository(User) private readonly userRepository: Repository<User>,
+    private jwtAuthService: JwtService,
   ) {}
 
   public async login(
@@ -27,10 +29,20 @@ export class AuthService {
     if (!user) throw new NotFoundException('Usuario no encontrado');
     const success = await comparePasswords(user.password, userToLogin.password);
     if (!success) throw new UnauthorizedException();
+
+    const payload = {
+      id: user.id,
+      name: user.person.name,
+      surname: user.person.surname,
+      phoneNumber: user.person.phoneNumber,
+      email: user.person.email,
+      identification: user.person.identification,
+    };
+    const token = await this.jwtAuthService.signAsync(payload);
+
     return {
       success: true,
-      token:
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZGVudGlmaWNhdGlvbiI6IjEyMDcxNjI3MDAiLCJuYW1lIjoiVG9tbXkgQXBvbGluYXJpbyIsImlhdCI6MTUxNjIzOTAyMn0.JXwBq3zoGWkmUcMzLnEZix4FeoPSUOgxgaenVvrZri8',
+      token: token,
     };
   }
 }
